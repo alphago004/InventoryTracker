f9716d31cc7fa1e2fb531c045a09f4687703ce5c
alphago004 <sagarbhetuwal48@gmail.com>
2023-09-22 22:20:42 -0500

    simplified conditional

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,23 +17,27 @@ const Expenses = ({ items }) => {
 
   const totalAmount = Math.round(filteredExpenses.reduce((acc, expense) => acc + expense.amount, 0) * 10) / 10;
 
+  let expensesContent = <p style={{color: "white"}}>No Expense Found.</p>;
+  if (filteredExpenses.length > 0 ) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={items.id} // It's often recommended to use a unique identifier for keys, not the index.
+        expense={expense}
+        //title = {expense.title}
+        //amount = {expense.amount}
+        //date = {expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {filteredExpenses.length === 0 ? ( 
-      <p style={{color: "white"}}>No Expenses Found. </p> 
-      ): (  
-      filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={items.id} // It's often recommended to use a unique identifier for keys, not the index.
-          expense={expense}
-          //title = {expense.title}
-          //amount = {expense.amount}
-          //date = {expense.date}
-        />
-      ))
-    )}
+      <ExpensesFilter 
+      selected={filteredYear} 
+      onChangeFilter={filterChangeHandler}
+      />
+      {expensesContent}
       <div style={{color:"white"}}>Total Amount: ${totalAmount}</div>
     </Card>
     </div>
